e35d9fe4dfe0f7f942a8423634260ae0c37bdc71
Yura Zakaryan <yura.zakaryan@braint.systems>
2024-06-24 17:00:58 +0400

    fixed price format

--- src/components/screen/club-control/wrapper/control-notify/index.tsx
+++ src/components/screen/club-control/wrapper/control-notify/index.tsx
@@ -1,5 +1,6 @@
 import { IControlNotify } from '@components/screen/club-control/types';
 import { SpecialWord } from '@components/wrapper';
+import { formattedPrice } from '@utils';
 
 export const ControlNotify: React.FC<IControlNotify> = ({ endedTimer }) => {
   return (
@@ -8,7 +9,7 @@ export const ControlNotify: React.FC<IControlNotify> = ({ endedTimer }) => {
         Վերջացել է <SpecialWord>{endedTimer.title}</SpecialWord> ֊ի ժամանակը
       </h2>
       <p>
-        Գումարը։ <SpecialWord>{Math.round(endedTimer.pricePerHour)} դրամ</SpecialWord>
+        Գումարը։ <SpecialWord>{formattedPrice(Math.round(endedTimer?.pricePerHour))} դրամ</SpecialWord>
       </p>
       <p>
         Ժամանակը։ <SpecialWord>{endedTimer.defineTime} րոպե</SpecialWord>
